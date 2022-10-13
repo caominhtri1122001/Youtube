20a53872a6e0db199fa417fb7adbc37cf55a7abd
Cao Minh Tri <caominhtri112201@gmail.com>
2022-10-13 17:17:49 +0700

    [BE] Create Model and RestAPI

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/users");
+const videoRoutes = require("./routes/videos");
+const commentRoutes = require("./routes/comments");
+
 const app = express();
 
 const connect = () => {
@@ -13,6 +17,10 @@ const connect = () => {
             throw err;
         });
 };
+
+app.use("api/users", userRoutes);
+app.use("api/videos", videoRoutes);
+app.use("api/comments", commentRoutes);
 
 app.listen(8000, () => {
     connect();
