931487a5c4a55b933ba0a461b90c48efcc2e65ef
Cao Minh Tri <caominhtri112201@gmail.com>
2022-10-19 17:48:40 +0700

    [BE] JWT Cookie Authentication

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -8,6 +8,7 @@ const verifyToken = (req, res, next) => {
     jwt.verify(token, process.env.JWT, (err, user) => {
         if (err) return next(createError(403, "Token is not valid!"));
         req.user = user;
+        next();
     });
 };
 
