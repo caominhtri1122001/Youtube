7693adc585046cc71384c3e11fc5611ade0a11ec
Cao Minh Tri <caominhtri112201@gmail.com>
2022-11-01 16:02:05 +0700

    [BE] Comments API

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const verifyToken = require("../verifyToken");
+const commentController = require("../controllers/comment");
 
 const router = express.Router();
+
+router.post("/", verifyToken, commentController.addComment);
+
+router.delete("/:id", verifyToken, commentController.deleteComment);
+
+router.get("/:videoId", commentController.getComments);
 
 module.exports = router;
